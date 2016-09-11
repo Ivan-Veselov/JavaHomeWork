2bd14c0c4f6ce7809d61b53148a57e5e75f73c28
Ivan Veselov <veselov.iva@gmail.com>
2016-09-11 14:27:53 +0300

    Add LinkedList tests.

--- 01-HashTable/src/ru/spbau/bachelor2015/veselov/hw01/Tester.java
+++ 01-HashTable/src/ru/spbau/bachelor2015/veselov/hw01/Tester.java
@@ -2,8 +2,68 @@ package ru.spbau.bachelor2015.veselov.hw01;
 
 public class Tester {
     public static void main(String[] args) {
-        HashTable table = new HashTable(5);
+        // LinkedList usage
+        LinkedList fLinkedList = new LinkedList();
+        assert(fLinkedList.put("A", "a") == null);
+        assert(fLinkedList.put("B", "b") == null);
+        assert(fLinkedList.put("C", "c") == null);
 
-        System.out.println(table.size());
+        assert(fLinkedList.contains("A"));
+        assert(fLinkedList.contains("B"));
+        assert(fLinkedList.contains("C"));
+        assert(!fLinkedList.contains("D"));
+        assert(!fLinkedList.contains("a"));
+        assert(!fLinkedList.contains("b"));
+        assert(!fLinkedList.contains("c"));
+
+        assert(fLinkedList.get("A") == "a");
+        assert(fLinkedList.get("B") == "b");
+        assert(fLinkedList.get("C") == "c");
+        assert(fLinkedList.get("D") == null);
+
+        assert(fLinkedList.remove("B") == "b"); // !
+
+        assert(fLinkedList.contains("A"));
+        assert(!fLinkedList.contains("B"));
+        assert(fLinkedList.contains("C"));
+
+        assert(fLinkedList.get("A") == "a");
+        assert(fLinkedList.get("B") == null);
+        assert(fLinkedList.get("C") == "c");
+
+        assert(fLinkedList.put("C", "cc") == "c"); // !
+
+        assert(fLinkedList.contains("A"));
+        assert(!fLinkedList.contains("B"));
+        assert(fLinkedList.contains("C"));
+
+        assert(fLinkedList.get("A") == "a");
+        assert(fLinkedList.get("B") == null);
+        assert(fLinkedList.get("C") == "cc");
+
+        assert(fLinkedList.remove("B") == null);
+
+        assert(fLinkedList.remove("C") == "cc"); // !
+
+        assert(fLinkedList.contains("A"));
+        assert(!fLinkedList.contains("C"));
+
+        assert(fLinkedList.get("A") == "a");
+        assert(fLinkedList.get("C") == null);
+
+        fLinkedList.clear(); // !
+
+        assert(!fLinkedList.contains("A"));
+        assert(fLinkedList.get("A") == null);
+
+        try {
+            fLinkedList.put(null, null);
+            assert(false);
+        } catch (IllegalArgumentException e) {}
+
+        // HashTable usage
+        HashTable fHashTable = new HashTable(5);
+
+        System.out.println(fHashTable.size());
     }
 }
