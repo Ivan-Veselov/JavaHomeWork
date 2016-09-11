c686263aa89ba199074b211cf80a4abce9746392
Ivan Veselov <veselov.iva@gmail.com>
2016-09-11 15:10:51 +0300

    Add size method to HashTable.

--- 01-HashTable/src/ru/spbau/bachelor2015/veselov/hw01/HashTable.java
+++ 01-HashTable/src/ru/spbau/bachelor2015/veselov/hw01/HashTable.java
@@ -2,6 +2,7 @@ package ru.spbau.bachelor2015.veselov.hw01;
 
 public class HashTable {
     private LinkedList[] mBuckets;
+    private int mSize = 0;
 
     public HashTable(int aBucketsNumber) {
         if (aBucketsNumber <= 0) {
@@ -12,9 +13,7 @@ public class HashTable {
     }
 
     public int size() {
-        // TODO
-
-        return 0;
+        return mSize;
     }
 
     public boolean contains(String aKey) {
@@ -26,17 +25,33 @@ public class HashTable {
     }
 
     public String put(String aKey, String aValue) {
-        return bucket(aKey).put(aKey, aValue);
+        LinkedList fBucket = bucket(aKey);
+
+        int fSizeAlteration = -fBucket.size();
+        String fPreviousValue = fBucket.put(aKey, aValue);
+        fSizeAlteration += fBucket.size();
+
+        mSize += fSizeAlteration;
+        return fPreviousValue;
     }
 
     public String remove(String aKey) {
-        return bucket(aKey).remove(aKey);
+        LinkedList fBucket = bucket(aKey);
+
+        int fSizeAlteration = -fBucket.size();
+        String fPreviousValue = fBucket.remove(aKey);
+        fSizeAlteration += fBucket.size();
+
+        mSize += fSizeAlteration;
+        return fPreviousValue;
     }
 
     public void clear() {
         for (LinkedList fBucket : mBuckets) {
             fBucket.clear();
         }
+
+        mSize = 0;
     }
 
     private LinkedList bucket(String aKey) {
