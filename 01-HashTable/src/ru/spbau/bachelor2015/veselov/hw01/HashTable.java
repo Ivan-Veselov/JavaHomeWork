a2be3b5884d2558d3b4ea6454f58c83941371ac2
Ivan Veselov <veselov.iva@gmail.com>
2016-09-11 14:52:19 +0300

    Add partial realization of HashTable.

--- 01-HashTable/src/ru/spbau/bachelor2015/veselov/hw01/HashTable.java
+++ 01-HashTable/src/ru/spbau/bachelor2015/veselov/hw01/HashTable.java
@@ -1,8 +1,14 @@
 package ru.spbau.bachelor2015.veselov.hw01;
 
 public class HashTable {
+    private LinkedList[] mBuckets;
+
     public HashTable(int aBucketsNumber) {
-        // TODO
+        if (aBucketsNumber <= 0) {
+            throw new IllegalArgumentException("There must be positive number of buckets in HashTable!");
+        }
+
+        mBuckets = new LinkedList[aBucketsNumber];
     }
 
     public int size() {
@@ -12,30 +18,28 @@ public class HashTable {
     }
 
     public boolean contains(String aKey) {
-        // TODO
-
-        return false;
+        return bucket(aKey).contains(aKey);
     }
 
     public String get(String aKey) {
-        // TODO
-
-        return "";
+        return bucket(aKey).get(aKey);
     }
 
     public String put(String aKey, String aValue) {
-        // TODO
-
-        return "";
+        return bucket(aKey).put(aKey, aValue);
     }
 
     public String remove(String aKey) {
-        // TODO
-
-        return "";
+        return bucket(aKey).remove(aKey);
     }
 
     public void clear() {
-        // TODO
+        for (LinkedList fBucket : mBuckets) {
+            fBucket.clear();
+        }
+    }
+
+    private LinkedList bucket(String aKey) {
+        return mBuckets[aKey.hashCode() % mBuckets.length];
     }
 }
