9977558369512355053117349965bd6e6bd0ec94
Ivan Veselov <veselov.iva@gmail.com>
2016-09-17 15:31:37 +0300

    Add Javadoc commentaries.

--- 01-HashTable/src/ru/spbau/bachelor2015/veselov/hw01/HashTable.java
+++ 01-HashTable/src/ru/spbau/bachelor2015/veselov/hw01/HashTable.java
@@ -1,5 +1,9 @@
 package ru.spbau.bachelor2015.veselov.hw01;
 
+/**
+ * A collection of String keys with String values associated with each key.
+ * Implemented as extendable hash table on keys.
+ */
 public class HashTable implements KeyValueMap {
     private LinkedList[] mBuckets;
     private int mSize = 0;
@@ -13,6 +17,11 @@ public class HashTable implements KeyValueMap {
         }
     }
 
+    /**
+     * Constructs empty hash table.
+     *
+     * @param aBucketsNumber a number of buckets in hash table
+     */
     public HashTable(int aBucketsNumber) {
         if (aBucketsNumber <= 0) {
             throw new IllegalArgumentException("There must be positive number of buckets in HashTable!");
@@ -21,10 +30,22 @@ public class HashTable implements KeyValueMap {
         initializeBuckets(aBucketsNumber);
     }
 
+    /**
+     * Returns size of hash table.
+     *
+     * @return number of elements currently stored in hash table
+     */
     public int size() {
         return mSize;
     }
 
+    /**
+     * Checks whether hash table contains element with a given key.
+     * Complexity: constant on average; linear in hash table size in worst case
+     *
+     * @param aKey a key to search for
+     * @return true if hash table contains element with key equal to aKey, false otherwise
+     */
     public boolean contains(String aKey) {
         if (aKey == null) {
             throw new IllegalArgumentException(sNullKeyExceptionMsg);
@@ -33,6 +54,14 @@ public class HashTable implements KeyValueMap {
         return bucket(aKey).contains(aKey);
     }
 
+    /**
+     * Returns value associated with a given key or null if there is no
+     * element with such a key in hash table.
+     * Complexity: constant on average; linear in hash table size in worst case
+     *
+     * @param aKey a key to search for
+     * @return value associated with aKey or null if there is no element with key equal to aKey
+     */
     public String get(String aKey) {
         if (aKey == null) {
             throw new IllegalArgumentException(sNullKeyExceptionMsg);
@@ -41,6 +70,16 @@ public class HashTable implements KeyValueMap {
         return bucket(aKey).get(aKey);
     }
 
+    /**
+     * Associates a given value with a given key by putting such a pair in hash table or
+     * if there is already an element with such a key then replaces previous value
+     * with a new one.
+     * Complexity: constant on average; linear in hash table size in worst case
+     *
+     * @param aKey a key which will be put in hash table or which value will be replaced
+     * @param aValue a value to associate with aKey
+     * @return previous value that was associated with aKey or null if aKey was not presented in hash table before
+     */
     public String put(String aKey, String aValue) {
         if (aKey == null) {
             throw new IllegalArgumentException(sNullKeyExceptionMsg);
@@ -61,6 +100,13 @@ public class HashTable implements KeyValueMap {
         return fPreviousValue;
     }
 
+    /**
+     * Removes element with a given key from hash table.
+     * Complexity: constant on average; linear in hash table size in worst case
+     *
+     * @param aKey a key of element that will be removed
+     * @return a value associated with aKey or null if there is no element with a key equal to aKey
+     */
     public String remove(String aKey) {
         if (aKey == null) {
             throw new IllegalArgumentException(sNullKeyExceptionMsg);
@@ -76,6 +122,10 @@ public class HashTable implements KeyValueMap {
         return fPreviousValue;
     }
 
+    /**
+     * Removes all elements from hash table.
+     * Complexity: linear in hash table size
+     */
     public void clear() {
         for (LinkedList fBucket : mBuckets) {
             fBucket.clear();
--- 01-HashTable/src/ru/spbau/bachelor2015/veselov/hw01/LinkedList.java
+++ 01-HashTable/src/ru/spbau/bachelor2015/veselov/hw01/LinkedList.java
@@ -4,20 +4,39 @@ import java.lang.IllegalArgumentException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * A collection of String keys with String values associated with each key.
+ * Implemented as linked list of pairs.
+ */
 public class LinkedList implements Iterable<KeyValuePair>, KeyValueMap {
     private LinkedListNode mHeadNode;
     private int mSize = 0;
 
     static private String sNullKeyExceptionMsg = "LinkedList key must not be null!";
 
+    /**
+     * Creates empty list.
+     */
     public LinkedList() {
         mHeadNode = new LinkedListNode();
     }
 
+    /**
+     * Returns size of list.
+     *
+     * @return number of elements currently stored in list
+     */
     public int size() {
         return mSize;
     }
 
+    /**
+     * Checks whether list contains element with a given key.
+     * Complexity: linear in list size
+     *
+     * @param aKey a key to search for
+     * @return true if list contains element with key equal to aKey, false otherwise
+     */
     public boolean contains(String aKey) {
         if (aKey == null) {
             throw new IllegalArgumentException(sNullKeyExceptionMsg);
@@ -26,6 +45,14 @@ public class LinkedList implements Iterable<KeyValuePair>, KeyValueMap {
         return findPredecessor(aKey).next() != null;
     }
 
+    /**
+     * Returns value associated with a given key or null if there is no
+     * element with such a key in list.
+     * Complexity: linear in list size
+     *
+     * @param aKey a key to search for
+     * @return value associated with aKey or null if there is no element with key equal to aKey
+     */
     public String get(String aKey) {
         if (aKey == null) {
             throw new IllegalArgumentException(sNullKeyExceptionMsg);
@@ -35,6 +62,16 @@ public class LinkedList implements Iterable<KeyValuePair>, KeyValueMap {
         return fNode != null ? fNode.element().value() : null;
     }
 
+    /**
+     * Associates a given value with a given key by putting such a pair in list or
+     * if there is already an element with such a key then replaces previous value
+     * with a new one.
+     * Complexity: linear in list size
+     *
+     * @param aKey a key which will be put in list or which value will be replaced
+     * @param aValue a value to associate with aKey
+     * @return previous value that was associated with aKey or null if aKey was not presented in list before
+     */
     public String put(String aKey, String aValue) {
         if (aKey == null) {
             throw new IllegalArgumentException(sNullKeyExceptionMsg);
@@ -53,6 +90,13 @@ public class LinkedList implements Iterable<KeyValuePair>, KeyValueMap {
         return null;
     }
 
+    /**
+     * Removes element with a given key from list.
+     * Complexity: linear in list size
+     *
+     * @param aKey a key of element that will be removed
+     * @return a value associated with aKey or null if there is no element with a key equal to aKey
+     */
     public String remove(String aKey) {
         if (aKey == null) {
             throw new IllegalArgumentException(sNullKeyExceptionMsg);
@@ -71,6 +115,10 @@ public class LinkedList implements Iterable<KeyValuePair>, KeyValueMap {
         return fValue;
     }
 
+    /**
+     * Removes all elements from list.
+     * Complexity: linear in list size
+     */
     public void clear() {
         mHeadNode.setNext(null);
         mSize = 0;
