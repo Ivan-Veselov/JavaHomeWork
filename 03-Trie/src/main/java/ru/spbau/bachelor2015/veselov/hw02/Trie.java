7fee8be714ab21e0d189c103c998fdaef725cad7
Ivan Veselov <veselov.iva@gmail.com>
2016-09-24 20:35:43 +0300

    Initial commit with IDEA gradle project and Trie class skeleton.

---
+++ 03-Trie/src/main/java/ru/spbau/bachelor2015/veselov/hw02/Trie.java
@@ -0,0 +1,27 @@
+package ru.spbau.bachelor2015.veselov.hw02;
+
+/**
+ * Trie structure which can maintain set of Strings and count Strings
+ * which begin with a given prefix. Also supports serialization.
+ */
+public class Trie {
+    public boolean add(String element) {
+        return false;
+    }
+
+    boolean contains(String element) {
+        return false;
+    }
+
+    boolean remove(String element) {
+        return false;
+    }
+
+    int size() {
+        return 0;
+    }
+
+    int howManyStartsWithPrefix(String prefix) {
+        return 0;
+    }
+}
