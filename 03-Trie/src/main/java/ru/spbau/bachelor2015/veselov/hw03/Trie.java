6daddbc4d1822131ceb4e4b2d0d445e137fb39c5
Ivan Veselov <veselov.iva@gmail.com>
2016-09-24 22:12:22 +0300

    Add Node inner class.

--- 03-Trie/src/main/java/ru/spbau/bachelor2015/veselov/hw03/Trie.java
+++ 03-Trie/src/main/java/ru/spbau/bachelor2015/veselov/hw03/Trie.java
@@ -1,10 +1,14 @@
 package ru.spbau.bachelor2015.veselov.hw03;
 
+import java.util.HashMap;
+
 /**
  * Trie structure which can maintain set of Strings and count Strings
  * which begin with a given prefix. Also supports serialization.
  */
 public class Trie {
+    private Node rootNode;
+
     public boolean add(String element) {
         return false;
     }
@@ -23,5 +27,50 @@ public class Trie {
 
     public int howManyStartsWithPrefix(String prefix) {
         return 0;
+    }
+
+    static private class Node {
+        private Node parentNode;
+        private HashMap<Character, Node> childNodes = new HashMap<>();
+        private boolean isTerminal = false;
+        private int numberOfTerminalsInSubTree = 0;
+
+        public Node(Node parentNode) {
+            this.parentNode = parentNode;
+        }
+
+        public Node getChildNode(Character edgeCharacter) {
+            Node child = childNodes.get(edgeCharacter);
+
+            if (child == null) {
+                child = new Node(this);
+                childNodes.put(edgeCharacter, child);
+            }
+
+            return child;
+        }
+
+        public void setTerminal(boolean isTerminal) {
+            if (this.isTerminal == isTerminal) {
+                return;
+            }
+
+            this.isTerminal = isTerminal;
+            final int alteration = isTerminal ? 1 : -1;
+
+            Node node = this;
+            while (node != null) {
+                node.numberOfTerminalsInSubTree += alteration;
+                node = node.parentNode;
+            }
+        }
+
+        public boolean isTerminal() {
+            return isTerminal;
+        }
+
+        public int getNumberOfTerminalsInSubTree() {
+            return numberOfTerminalsInSubTree;
+        }
     }
 }
