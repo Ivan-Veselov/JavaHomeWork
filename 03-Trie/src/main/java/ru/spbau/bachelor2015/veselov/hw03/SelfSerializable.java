62f676310fdf88eb878b2cc89918ba3f188f7b41
Ivan Veselov <veselov.iva@gmail.com>
2016-09-25 01:22:37 +0300

    Add SelfSerializable interface.

---
+++ 03-Trie/src/main/java/ru/spbau/bachelor2015/veselov/hw03/SelfSerializable.java
@@ -0,0 +1,14 @@
+package ru.spbau.bachelor2015.veselov.hw03;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Serializable;
+
+/**
+ * Interface of a class which can serialize and deserialize itself.
+ */
+public interface SelfSerializable extends Serializable {
+    void serialize(OutputStream out) throws IOException;
+    void deserialize(InputStream in) throws IOException;
+}
