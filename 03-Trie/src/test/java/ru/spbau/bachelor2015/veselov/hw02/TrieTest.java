3baad6eae552a4c2dfee760241f57e87dcba666f
Ivan Veselov <veselov.iva@gmail.com>
2016-09-24 20:39:01 +0300

    Add test class skeleton.

---
+++ 03-Trie/src/test/java/ru/spbau/bachelor2015/veselov/hw02/TrieTest.java
@@ -0,0 +1,32 @@
+package ru.spbau.bachelor2015.veselov.hw02;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TrieTest {
+    @Test
+    public void add() throws Exception {
+
+    }
+
+    @Test
+    public void contains() throws Exception {
+
+    }
+
+    @Test
+    public void remove() throws Exception {
+
+    }
+
+    @Test
+    public void size() throws Exception {
+
+    }
+
+    @Test
+    public void howManyStartsWithPrefix() throws Exception {
+
+    }
+}
