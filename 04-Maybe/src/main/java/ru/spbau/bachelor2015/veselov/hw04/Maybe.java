f414172e60e8a2032b8f4c5f335f9014b7801102
Ivan Veselov <veselov.iva@gmail.com>
2016-09-28 15:45:31 +0300

    Add implementation to Maybe class. Add jetbrains annotations lib.

---
+++ 04-Maybe/src/main/java/ru/spbau/bachelor2015/veselov/hw04/Maybe.java
@@ -0,0 +1,37 @@
+package ru.spbau.bachelor2015.veselov.hw04;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Function;
+
+public class Maybe<T> {
+    private T object;
+
+    public static <T> Maybe<T> just(T t) {
+        return new Maybe<T> (t);
+    }
+
+    public static <T> Maybe<T> nothing() {
+        return new Maybe<T> (null);
+    }
+
+    public @NotNull T get() {
+        return object;
+    }
+
+    public boolean isPresent() {
+        return object != null;
+    }
+
+    public <U> Maybe<U> map(Function<T, U> mapper) {
+        if (!isPresent()) {
+            return Maybe.nothing();
+        }
+
+        return Maybe.just(mapper.apply(object));
+    }
+
+    private Maybe(T object) {
+        this.object = object;
+    }
+}
