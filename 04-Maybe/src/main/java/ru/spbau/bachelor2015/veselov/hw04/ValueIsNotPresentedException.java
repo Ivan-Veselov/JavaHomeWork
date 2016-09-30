aa1cd473e78d6cb8db15fad7a27324a782abd020
Ivan Veselov <veselov.iva@gmail.com>
2016-09-30 23:45:25 +0300

    Add Javadocs.

--- 04-Maybe/src/main/java/ru/spbau/bachelor2015/veselov/hw04/ValueIsNotPresentedException.java
+++ 04-Maybe/src/main/java/ru/spbau/bachelor2015/veselov/hw04/ValueIsNotPresentedException.java
@@ -1,5 +1,9 @@
 package ru.spbau.bachelor2015.veselov.hw04;
 
+/**
+ * An Exception which will be thrown in case when get method of non-instance Maybe
+ * object is called.
+ */
 public class ValueIsNotPresentedException extends RuntimeException {
     ValueIsNotPresentedException() {
         super();
