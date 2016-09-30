b5db55e487899e49a6415ee9b9141d6cc577dbcb
Ivan Veselov <veselov.iva@gmail.com>
2016-10-01 00:44:53 +0300

    Add BinaryTreeSet class skeleton.

---
+++ 04-Tree/src/main/java/ru/spbau/bachelor2015/veselov/hw04/BinaryTreeSet.java
@@ -0,0 +1,17 @@
+package ru.spbau.bachelor2015.veselov.hw04;
+
+public class BinaryTreeSet<T extends Comparable<? super T>> {
+    public BinaryTreeSet() {
+    }
+
+    public void add(T newElement) {
+    }
+
+    public boolean contains(T element) {
+        return false;
+    }
+
+    public int size() {
+        return 0;
+    }
+}
