ac7431fd4588fd37b4b5bcbdd1ad9a62a4e085b7
Ivan Veselov <veselov.iva@gmail.com>
2016-10-01 00:00:07 +0300

    Add SquareTranslator class skeleton.

---
+++ 04-Maybe/src/main/java/ru/spbau/bachelor2015/veselov/hw04/SquareTranslator.java
@@ -0,0 +1,9 @@
+package ru.spbau.bachelor2015.veselov.hw04;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class SquareTranslator {
+    public static void translate(InputStream in, OutputStream out) {
+    }
+}
