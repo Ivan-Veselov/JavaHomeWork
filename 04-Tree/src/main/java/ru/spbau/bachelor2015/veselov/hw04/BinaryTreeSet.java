e74b5103a3bb1ece5fa07b4fcd7d761a562845b8
Ivan Veselov <veselov.iva@gmail.com>
2016-10-01 01:01:33 +0300

    Add Node inner class.

--- 04-Tree/src/main/java/ru/spbau/bachelor2015/veselov/hw04/BinaryTreeSet.java
+++ 04-Tree/src/main/java/ru/spbau/bachelor2015/veselov/hw04/BinaryTreeSet.java
@@ -16,4 +16,14 @@ public class BinaryTreeSet<T extends Comparable<? super T>> {
     public int size() {
         return 0;
     }
+
+    static class Node<T extends Comparable<? super T>> {
+        private @NotNull T element;
+        Node<T> leftChild = null;
+        Node<T> rightChild = null;
+
+        private Node(@NotNull T element) {
+            this.element = element;
+        }
+    }
 }
