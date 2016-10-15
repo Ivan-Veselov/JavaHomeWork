0d0d5210376c0d3f1c6b92b96c048711be58e43e
Ivan Veselov <veselov.iva@gmail.com>
2016-10-15 21:18:59 +0300

    Add implementation for map method of Collections class.

--- 05-FP/src/main/java/ru/spbau/bachelor2015/veselov/hw05/Collections.java
+++ 05-FP/src/main/java/ru/spbau/bachelor2015/veselov/hw05/Collections.java
@@ -1,8 +1,45 @@
 package ru.spbau.bachelor2015.veselov.hw05;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Iterator;
+
+/**
+ * Class which provides functions which use Collections and Functional classes together.
+ */
 public class Collections {
-    public static void map() {
-        throw new UnsupportedOperationException();
+    /**
+     * Applies given function to all elements of iterable object and returns iterable object of results of application.
+     *
+     * @param function A function which is applied to elements.
+     * @param iterable An iterable object which represents a list of elements.
+     * @param <Source> Type of function argument.
+     * @param <Target> Type of function returning value.
+     * @param <T> Type of elements in iterable object.
+     * @return Iterable object which consists of results of application.
+     */
+    public static <Source, Target, T extends Source> Iterable<Target> map(
+            @NotNull Function1<Source, Target> function, @NotNull Iterable<T> iterable) {
+        return new Iterable<Target>() {
+            @Override
+            public Iterator<Target> iterator() {
+                return new IteratorWrapper();
+            }
+
+            class IteratorWrapper implements Iterator<Target> {
+                private Iterator<T> iterator = iterable.iterator();
+
+                @Override
+                public boolean hasNext() {
+                    return iterator.hasNext();
+                }
+
+                @Override
+                public Target next() {
+                    return function.apply(iterator.next());
+                }
+            }
+        };
     }
 
     public static void filter() {
