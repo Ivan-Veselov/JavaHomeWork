2620d1f3f89eb7d6a3f7c172a8ada6b3cc59a872
Ivan Veselov <veselov.iva@gmail.com>
2016-10-15 21:07:35 +0300

    Add skeleton of Collection class.

---
+++ 05-FP/src/main/java/ru/spbau/bachelor2015/veselov/hw05/Collections.java
@@ -0,0 +1,31 @@
+package ru.spbau.bachelor2015.veselov.hw05;
+
+public class Collections {
+    public static void map() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void filter() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void takeWhile() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void takeUnless() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void foldr() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void foldl() {
+        throw new UnsupportedOperationException();
+    }
+
+    private Collections() {
+        throw new UnsupportedOperationException();
+    }
+}
