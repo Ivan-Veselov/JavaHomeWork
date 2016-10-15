d7e8bc267a45102b1c0a19fcb0461b5b1e962d84
Ivan Veselov <veselov.iva@gmail.com>
2016-10-15 23:47:13 +0300

    Add skeleton of Collections testing class.

---
+++ 05-FP/src/test/java/ru/spbau/bachelor2015/veselov/hw05/CollectionsTest.java
@@ -0,0 +1,41 @@
+package ru.spbau.bachelor2015.veselov.hw05;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CollectionsTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testMap() throws Exception {
+
+    }
+
+    @Test
+    public void testFilter() throws Exception {
+
+    }
+
+    @Test
+    public void testTakeWhile() throws Exception {
+
+    }
+
+    @Test
+    public void testTakeUnless() throws Exception {
+
+    }
+
+    @Test
+    public void testFoldl() throws Exception {
+
+    }
+
+    @Test
+    public void testFoldr() throws Exception {
+
+    }
+}
