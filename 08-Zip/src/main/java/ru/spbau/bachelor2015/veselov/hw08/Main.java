6fdf79f7350e0fd956a463c8f761c6ba61edbb43
Ivan Veselov <veselov.iva@gmail.com>
2016-11-09 15:44:53 +0300

    Add methods skeletons.

--- 08-Zip/src/main/java/ru/spbau/bachelor2015/veselov/hw08/Main.java
+++ 08-Zip/src/main/java/ru/spbau/bachelor2015/veselov/hw08/Main.java
@@ -1,6 +1,20 @@
 package ru.spbau.bachelor2015.veselov.hw08;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.nio.file.Path;
+import java.util.regex.Pattern;
+import java.util.zip.ZipFile;
+
 public class Main {
     public static void main(String[] args) {
+    }
+
+    private static void unZip(@NotNull Path fileTree, @NotNull Pattern pattern) {
+        throw new UnsupportedOperationException();
+    }
+
+    private static void unZip(@NotNull ZipFile archive) {
+        throw new UnsupportedOperationException();
     }
 }
