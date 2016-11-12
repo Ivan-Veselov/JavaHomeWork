23a8939718e5dcb2503696f0e501ffbe0db55df6
Ivan Veselov <veselov.iva@gmail.com>
2016-11-12 12:28:20 +0300

    Implement unZip for fileTree traverse.

--- 08-Zip/src/main/java/ru/spbau/bachelor2015/veselov/hw08/Main.java
+++ 08-Zip/src/main/java/ru/spbau/bachelor2015/veselov/hw08/Main.java
@@ -2,15 +2,16 @@ package ru.spbau.bachelor2015.veselov.hw08;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.nio.file.InvalidPathException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
+import java.util.zip.ZipException;
 import java.util.zip.ZipFile;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         if (args.length != 2) {
             System.out.println("Invalid number of arguments! Must be 2.");
             return;
@@ -33,11 +34,42 @@ public class Main {
         unZip(path, pattern);
     }
 
-    private static void unZip(@NotNull Path fileTree, @NotNull Pattern pattern) {
-        throw new UnsupportedOperationException();
+    private static void unZip(@NotNull Path fileTree, @NotNull Pattern pattern) throws IOException {
+        Files.walkFileTree(fileTree,
+            new FileVisitor<Path>() {
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    try (ZipFile archive = new ZipFile(file.toFile())) {
+                        unZip(archive, pattern);
+                    } catch (ZipException e) {
+                        // Not a zip archive
+                    } catch (IOException e) {
+                        System.out.println(e.getMessage());
+                    }
+
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                    System.out.println(exc.getMessage());
+                    return FileVisitResult.SKIP_SUBTREE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                    return FileVisitResult.CONTINUE;
+                }
+            }
+        );
     }
 
-    private static void unZip(@NotNull ZipFile archive) {
+    private static void unZip(@NotNull ZipFile archive, @NotNull Pattern pattern) {
         throw new UnsupportedOperationException();
     }
 }
