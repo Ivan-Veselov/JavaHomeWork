a759a4e894e49a2295625a0cf65447629d6dfd96
Ivan Veselov <veselov.iva@gmail.com>
2016-11-12 12:07:03 +0300

    Add argument processing in main method.

--- 08-Zip/src/main/java/ru/spbau/bachelor2015/veselov/hw08/Main.java
+++ 08-Zip/src/main/java/ru/spbau/bachelor2015/veselov/hw08/Main.java
@@ -2,12 +2,35 @@ package ru.spbau.bachelor2015.veselov.hw08;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 import java.util.zip.ZipFile;
 
 public class Main {
     public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Invalid number of arguments! Must be 2.");
+            return;
+        }
+
+        Path path;
+        Pattern pattern;
+
+        try {
+            path = Paths.get(args[0]);
+            pattern = Pattern.compile(args[1]);
+        } catch (InvalidPathException e) {
+            System.out.println(e.getMessage());
+            return;
+        } catch (PatternSyntaxException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+
+        unZip(path, pattern);
     }
 
     private static void unZip(@NotNull Path fileTree, @NotNull Pattern pattern) {
