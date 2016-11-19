4c84ad79b1ae82d2541767ddb73c392dfeaa9946
Ivan Veselov <veselov.iva@gmail.com>
2016-11-19 14:39:00 +0300

    Implement serialize method.

--- 09-Serialization/src/main/java/ru/spbau/bachelor2015/veselov/hw09/Serialization.java
+++ 09-Serialization/src/main/java/ru/spbau/bachelor2015/veselov/hw09/Serialization.java
@@ -2,12 +2,47 @@ package ru.spbau.bachelor2015.veselov.hw09;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.lang.reflect.Field;
+import java.nio.charset.StandardCharsets;
 
 public class Serialization {
-    public static void serialize(@NotNull Object obj, @NotNull OutputStream stream) {
-        throw new UnsupportedOperationException();
+    public static void serialize(@NotNull Object obj, @NotNull OutputStream stream) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stream));
+
+        Class clazz = obj.getClass();
+        Field[] fields = clazz.getDeclaredFields();
+
+        for (Field field : fields) {
+            if (field.isSynthetic()) {
+                continue;
+            }
+
+            Object fieldValue;
+            try {
+                field.setAccessible(true);
+                fieldValue = field.get(obj);
+            } catch (IllegalAccessException e) {
+                /* This exception isn't supposed to be thrown, but if it is then problem is with this algorithm of
+                serialization.
+                 */
+                throw new RuntimeException(e);
+            }
+
+            if (fieldValue instanceof String) {
+                String string = (String) fieldValue;
+                for (byte b : string.getBytes(StandardCharsets.UTF_8)) {
+                    writer.write(Byte.toString(b));
+                    writer.write(" ");
+                }
+            } else {
+                writer.write(fieldValue.toString());
+            }
+
+            writer.newLine();
+        }
+
+        writer.flush();
     }
 
     public static <T> @NotNull T deserialize(@NotNull InputStream stream, @NotNull Class<T> clazz) {
